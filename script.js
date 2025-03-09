c5a0a799857573e3194fe6d5126efa124dd599cf
SYED MOHAMMAD SAMEER <141164589+samxiao0@users.noreply.github.com>
2025-03-09 14:00:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const colorMap = {
 async function startWebcam() {
     try {
         let stream = await navigator.mediaDevices.getUserMedia({
-            video: { width: 340, height: 220 }
+            video: { width: 320, height: 240 }
         });
         video.srcObject = stream;
     } catch (error) {
